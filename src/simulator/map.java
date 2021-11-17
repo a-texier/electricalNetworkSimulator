6cd704abe453a0d0e0869834b0a85ca6af189de4
a-texier <you@example.com>
2021-11-17 22:36:43 +0100

    start

---
+++ src/simulator/map.java
@@ -0,0 +1,33 @@
+package simulator;
+import profiles.ProfilesGroup;
+import profiles.Profile;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class map {
+
+    private int[][] tab;
+    private HashMap<City, Integer> map = new HashMap<City, Integer>();
+
+
+    public map(int[][] tab, HashMap<City,Integer> map) {
+        this.tab = tab;
+        this.map = map;
+    }
+
+    public ArrayList<Integer> road (City city1, City City2){
+
+        ArrayList<Integer>  road = new ArrayList<Integer>();  
+        
+
+
+
+
+        return road;
+    }
+
+
+
+    
+}
